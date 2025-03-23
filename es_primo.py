acecbe00750cdabe5e19c2180a874a1f547f2f4c
jesusa-sanchez-git <jesusa.sanchez@iteso.mx_PianoMan11VJ>
2025-03-23 02:08:21 -0600

    First

--- es_primo.py
+++ es_primo.py
@@ -1,15 +1,14 @@
-"""
-Inserta el encabezado aquí y escribe tu código abajo
-"""
-
-# Declaraciones
-CONSTANTE = valor
-
-# Entradas
-entrada = input()
-
-# Proceso
-
-
-# Salidas
-print(salida)
+import math
+def Es_primo(N):
+    if N<2:
+        return False
+    for i in range(2, int(math.sqrt(N)) + 1):
+        if N % i == 0:
+            return False
+        i += 1
+    return True
+N = int(input("Ingrese el numero que quisiera saber si es primo : \n"))
+P = True
+i= 0
+Primo = Es_primo(N)
+print(f"El número {N} {'si' if Primo else 'no'} es primo.")
